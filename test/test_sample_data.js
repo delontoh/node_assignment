f661f60bc51f69d770b14a6a36ca5668357af8e7
delontoh <delon@zave.sg>
2020-04-08 20:52:05 +0800

    1.0/node_assignment: added test_data reference.

---
+++ test/test_sample_data.js
@@ -0,0 +1,16 @@
+// Dummy data for test purpose
+
+// *** Teacher student relations ***
+
+const TEST_DATA = [
+    {
+        "teacher": "teacherken@gmail.com",
+        "students": ["studenthon@example.com", "studentjon@example.com"]
+    },
+    {
+        "teacher": "teacherdelontoh@gmail.com",
+        "students":["studentrebecca@example.com", "studentfranka@example.com", "studentliskarm@example.com", "studentsilverash@example.com"]
+    }
+];
+
+
